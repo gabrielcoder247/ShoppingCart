2fb7fd699deb639108c15ed30d27b1822fa017dc
gabrielcoder247 <gabrielcoder247@gmail.com>
2020-12-08 14:47:49 +0100

    creating orders with redux

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,6 +5,7 @@ import Modal from "react-modal";
 import Zoom from "react-reveal/Zoom";
 import { connect } from "react-redux";
 import { fetchProducts } from "../actions/productActions";
+import { addToCart } from "../actions/cartActions";
 
 class Products extends Component {
   constructor(props) {
@@ -103,5 +104,7 @@ export default connect(
   (state) => ({ products: state.products.filteredItems }),
   {
     fetchProducts,
+    addToCart,
   }
-)(Products);
+)
+(Products);
