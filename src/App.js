79d35d3028e636fba01fac89c32bebbcd68a9372
gabrielcoder247 <gabrielcoder247@gmail.com>
2020-12-08 18:51:52 +0100

    create admin screen for managing orders

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React from "react";
-import Products from "./components/Products";
-import Filter from "./components/Filter";
-import Cart from "./components/Cart";
 import store from "./store";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import AdminScreen from "./screens/AdminScreen";
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="grid-container">
-          <header>
-            <a href="/">Shopping Cart</a>
-          </header>
-          <main>
-            <div className="content">
-              <div className="main">
-                <Filter></Filter>
-                <Products></Products>
-              </div>
-              <div className="sidebar">
-                <Cart />
-              </div>
-            </div>
-          </main>
-          <footer>Alright is reserved &copy; 2020. Made with 💚  by Gabriel</footer>
-        </div>
+        <BrowserRouter>
+          <div className="grid-container">
+            <header>
+              <Link to="/">Shopping Cart</Link>
+              <Link to="/admin"></Link>
+            </header>
+            <main>
+              <Route path="/admin" component={AdminScreen} />
+              <Route path="/" component={HomeScreen} exact />
+            </main>
+            <footer>Alright is reserved &copy; 2020. Made with 💚  by Gabriel</footer>
+          </div>
+        </BrowserRouter>
       </Provider>
     );
   }
